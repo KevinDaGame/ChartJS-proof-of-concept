a764248c3f5d4a6663dfcdf39b5f6bad9b848977
Kevin Dolfing <Kevindolfing2@gmail.com>
2021-10-24 12:39:50 +0200

    commt

---
+++ ChartJS proof of concept/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ChartJS_proof_of_concept.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChartJS_proof_of_concept.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Circle = new int[] { };
+            ViewBag.StackedBar = new int[] { };
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChartJS proof of concept/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChartJS_proof_of_concept.Models
+{
+    public class StackedBarViewModel
+    {
+
+    }
+}
